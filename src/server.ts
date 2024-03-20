eb1a95689d4c5889f9ccd14ccf82ba454c88a3a6
David <dfl8@aluno.ifal.edu.br>
2024-03-20 15:51:41 -0300

    feat: add error treatments

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,42 @@
 import fastify from "fastify";
 import { z } from "zod";
 import { sql } from "./lib/postgres";
+import postgres from "postgres";
 
 const app = fastify();
 const PORT = 3333;
 
 app.post("/links", async (request, reply) => {
-    const createLinkSchema = z.object({
-        code: z.string().min(3),
-        url: z.string().url()
+    try {
+        const createLinkSchema = z.object({
+            code: z.string().min(3),
+            url: z.string().url()
+        })
+    
+        const { code, url } = createLinkSchema.parse(request.body);
+    
+        const result = await sql/*sql*/`
+            INSERT INTO short_links (code, original_url)
+            VALUES (${code}, ${url})
+            RETURNING id
+        `
+    
+        const link = result[0]
+    
+        return reply.status(201).send({ shortlinkId: link.id});
+    } catch (err) {
+        if (err instanceof postgres.PostgresError) {
+            if (err.code === "23505") {
+                return reply.status(400).send({
+                    message: "Duplicated code! This code already exists."
+                })
+            }
+        }
+    }
+
+    return reply.status(500).send({
+        message: "Internal server error."
     })
-
-    const { code, url } = createLinkSchema.parse(request.body);
-
-    const result = await sql/*sql*/`
-        INSERT INTO short_links (code, original_url)
-        VALUES (${code}, ${url})
-        RETURNING id
-    `
-
-    const link = result[0]
-
-    return reply.status(201).send({ shortlinkId: link.id});
 })
 
 app.listen({
