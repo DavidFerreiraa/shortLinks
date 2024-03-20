f54caea11548cfdfcf0beae33ebab7c8ff18d216
David <dfl8@aluno.ifal.edu.br>
2024-03-20 15:07:34 -0300

    feat: add create links route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { sql } from "./lib/postgres";
 
 const app = fastify();
 const PORT = 3333;
 
-app.get("/teste", () => console.log("hello world"))
+app.post("/links", async (request, reply) => {
+    const createLinkSchema = z.object({
+        code: z.string().min(3),
+        url: z.string().url()
+    })
+
+    const { code, url } = createLinkSchema.parse(request.body);
+
+    const result = await sql/*sql*/`
+        INSERT INTO short_links (code, original_url)
+        VALUES (${code}, ${url})
+        RETURNING id
+    `
+
+    const link = result[0]
+
+    return reply.status(201).send({ shortlinkId: link.id});
+})
 
 app.listen({
     port: PORT,
